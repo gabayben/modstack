c12143839b7ccdb36b3ebb685bf795be344ea778
bengabay <ben@netop.cloud>
2024-05-18 19:32:46 +0300

    added include to Flow.add_module(...)

--- modstack/modstack/engines/engine.py
+++ modstack/modstack/engines/engine.py
@@ -31,19 +31,23 @@ class Engine(EngineBase):
         self,
         name: str,
         module: TModule,
+        include: list[str] | None = None,
         flatten_single: bool = True
     ) -> TModule:
         if name in self.modules:
-            raise EngineModuleError(f"Module '{name}' already exists.")
+            raise EngineModuleError(f"Module '{name}' already exists in engine.")
         self.modules[name] = module
 
         endpoints: list[Endpoint] = list(module.endpoints.values())
+        if include:
+            endpoints = [endpoint for endpoint in endpoints if endpoint.name in include]
+
         if flatten_single and len(endpoints) == 1:
             try:
                 self.add_endpoint(endpoints[0], path=name)
             except EngineEndpointError as e:
                 raise EngineEndpointError(
-                    f"Endpoint '{name}' already exists. Set `flatten_single` to False "
+                    f"Endpoint '{name}' already exists in engine. Set `flatten_single` to False "
                     f"in order to include both the module and endpoint names in the path."
                 ) from e
         else:
@@ -56,7 +60,7 @@ class Engine(EngineBase):
     def add_endpoint(self, endpoint: Endpoint, path: str | None = None) -> None:
         path = path or endpoint.name
         if path in self.endpoints:
-            raise EngineEndpointError(f"Endpoint '{path}' already exists.")
+            raise EngineEndpointError(f"Endpoint '{path}' already exists in engine.")
         self.endpoints[path] = endpoint
 
     def get_endpoint(
@@ -65,7 +69,7 @@ class Engine(EngineBase):
         output_type: Type[Out] = Any
     ) -> Endpoint[Out]:
         if path not in self.endpoints:
-            raise EngineEndpointError(f"Endpoint '{path}' does not exist.")
+            raise EngineEndpointError(f"Endpoint '{path}' not found in engine.")
         return self.endpoints[path]
 
 class EngineContext(EngineBase):
