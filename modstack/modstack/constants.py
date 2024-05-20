afe7938743bdd204cde0355672e0e1b7052071c4
bengabay <ben@netop.cloud>
2024-05-20 13:07:37 +0300

    hihi

--- modstack/modstack/constants.py
+++ modstack/modstack/constants.py
@@ -1,5 +1,1 @@
 VARIADIC_TYPE = '__variadic_type__'
-MODSTACK_ENDPOINT = '__modstack_endpoint__'
-MODSTACK_RUNNABLE = '__modstack_runnable__'
-IGNORE_INPUT_SCHEMA = '__ignore_input_schema__'
-IGNORE_OUTPUT_SCHEMA = '__ignore_output_schema__'
