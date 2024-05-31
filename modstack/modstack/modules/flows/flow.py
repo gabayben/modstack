fc7883629c88005450ecff0074eb094a17678ac5
bengabay <ben@netop.cloud>
2024-06-01 01:35:14 +0300

    working on Flow

--- modstack/modstack/modules/flows/flow.py
+++ modstack/modstack/modules/flows/flow.py
@@ -10,8 +10,34 @@ from modstack.modules.flows import FlowBase, FlowData, RunFlow
 from modstack.typing import Effect, Effects
 
 class Flow(FlowBase):
-    def forward(self, data: RunFlow, **kwargs) -> Effect[FlowData]:
-        return Effects.Sync(partial(self._invoke, data, **kwargs))
+    def forward(self, contract: RunFlow, **kwargs) -> Effect[FlowData]:
+        return Effects.Sync(partial(self._invoke, contract, **kwargs))
 
-    def _invoke(self, data: RunFlow, **kwargs) -> FlowData:
-        pass
+    def _invoke(self, contract: RunFlow, **kwargs) -> FlowData:
+        # reset the visits count for each node
+        self._init_graph()
+
+        # normalize data
+        data = self._match_flow_inputs(contract.data)
+
+        # Raise if input is malformed in some way
+        self._validate_inputs(data)
+
+        # Initialize the inputs state
+        last_inputs = self._init_inputs_state(data)
+
+        # Take all nodes that have at least 1 input not connected or is variadic,
+        # and all nodes that have no inputs at all
+        to_run = self._init_to_run()
+
+        # These variables are used to detect when we're stuck in a loop.
+        # Stuck loops can happen when one or more nodes are waiting for input but
+        # no other node is going to run.
+        # This can happen when a whole branch of the graph is skipped for example.
+        # When we find that there are two consecutive iterations of the loop where waiting_for_input is the same,
+        # we know we're stuck in a loop and we can't make any progress.
+        before_last_waiting_for_input: set[str] | None = None
+        last_waiting_for_input: set[str] | None = None
+
+        # this is what we'll return at the end
+        final_outputs: FlowData = {}
