ce3012eda3d24a3c4378202e91024dbc89f814b9
bengabay <ben@netop.cloud>
2024-06-06 11:46:01 +0300

    filtering

--- modstack/modstack/typing/__init__.py
+++ modstack/modstack/typing/__init__.py
@@ -33,6 +33,14 @@ from .types import (
 
 from .protocols import Addable
 from .addable_dict import AddableDict
-from .effect import Effect, Effects, ReturnType
 from .serializable import Serializable
+from .effect import Effect, Effects, ReturnType
 from .registry import PydanticRegistry
+
+from .filtering import (
+    FilterOperator,
+    FilterCondition,
+    MetadataFilter,
+    MetadataFilters,
+    MetadataFilterInfo
+)
---
+++ modstack/modstack/typing/filtering.py
@@ -0,0 +1,38 @@
+from enum import StrEnum
+from typing import Union
+
+from pydantic import StrictFloat, StrictInt, StrictStr
+
+from modstack.typing import Serializable
+
+class FilterOperator(StrEnum):
+    EQ = '=='
+    NE = '!='
+    GT = '>'
+    GTE = '>='
+    LT = '<'
+    LTE = '<='
+    IN = 'in'
+    NIN = 'nin'
+    ANY = 'any'
+    ALL = 'all'
+    CONTAINS = 'contains'
+    TEXT_MATCH = 'text_match'
+
+class FilterCondition(StrEnum):
+    AND = 'and'
+    OR = 'or'
+
+class MetadataFilter(Serializable):
+    key: str
+    value: Union[StrictInt, StrictFloat, StrictStr, list[StrictStr]]
+    operator: FilterOperator = FilterOperator.EQ
+
+class MetadataFilters(Serializable):
+    filters: Union[MetadataFilter, 'MetadataFilters']
+    condition: FilterCondition = FilterCondition.AND
+
+class MetadataFilterInfo(Serializable):
+    name: str
+    type: str
+    description: str
