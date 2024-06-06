9c1bedd3b31f792baee6993871101650a3888b41
bengabay <ben@netop.cloud>
2024-06-07 00:38:11 +0300

    added modstack-redis

---
+++ libs/integrations/redis/modstack/redis/kv_store.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+from modstack.stores.keyvalue import KVStore
+from modstack.stores.keyvalue.base import DEFAULT_COLLECTION
+
+class RedisKVStore(KVStore):
+    def get(self, key: str, collection: str = DEFAULT_COLLECTION, **kwargs) -> Optional[dict]:
+        pass
+
+    async def aget(self, key: str, collection: str = DEFAULT_COLLECTION, **kwargs) -> Optional[dict]:
+        pass
+
+    def get_all(self, collection: str = DEFAULT_COLLECTION, **kwargs) -> dict[str, dict]:
+        pass
+
+    async def aget_all(self, collection: str = DEFAULT_COLLECTION, **kwargs) -> dict[str, dict]:
+        pass
+
+    def put(self, key: str, value: dict, collection: str = DEFAULT_COLLECTION, **kwargs) -> None:
+        pass
+
+    async def aput(self, key: str, value: dict, collection: str = DEFAULT_COLLECTION, **kwargs) -> None:
+        pass
+
+    def delete(self, key: str, collection: str = DEFAULT_COLLECTION, **kwargs) -> bool:
+        pass
+
+    async def adelete(self, key: str, collection: str = DEFAULT_COLLECTION, **kwargs) -> bool:
+        pass
