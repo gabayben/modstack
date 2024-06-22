2055576ff2624098f15c28d5ec126cfd80ac38ce
bengabay <ben@netop.cloud>
2024-06-22 05:37:11 +0300

    Effects.Parallel for now

--- modstack/modstack/typing/effect.py
+++ modstack/modstack/typing/effect.py
@@ -4,7 +4,7 @@ from typing import Any, AsyncIterator, Callable, Coroutine, Generic, Iterator, O
 
 from modstack.typing.protocols import Addable
 from modstack.typing.vars import Other, Out
-from modstack.utils.func import tzip
+from modstack.utils.func import tpartial, tzip
 from modstack.utils.threading import get_executor, run_async, run_async_iter, run_sync, run_sync_iter
 
 class Effect(Generic[Out], ABC):
@@ -185,10 +185,10 @@ class Effects:
             if self._invoke:
                 return self._invoke()
             elif self._ainvoke:
-                return self._aforward().invoke()
+                return self._async_effect().invoke()
             elif self._iter:
-                return self._iter_forward().invoke()
-            return self._aiter_forward().invoke()
+                return self._iter_effect().invoke()
+            return self._async_iter_effect().invoke()
 
         async def ainvoke(self) -> Out:
             if self._ainvoke:
@@ -196,8 +196,8 @@ class Effects:
             elif self._invoke:
                 return await run_async(self._invoke)
             elif self._aiter:
-                return await self._aiter_forward().ainvoke()
-            return await self._iter_forward().ainvoke()
+                return await self._async_iter_effect().ainvoke()
+            return await self._iter_effect().ainvoke()
 
         def iter(self) -> Iterator[Out]:
             if self._iter:
@@ -206,7 +206,7 @@ class Effects:
                 yield from run_sync_iter(self._aiter)
             elif self._invoke:
                 yield self._invoke()
-            yield self._aforward().invoke()
+            yield self._async_effect().invoke()
 
         async def aiter(self) -> AsyncIterator[Out]:
             if self._aiter:
@@ -219,13 +219,13 @@ class Effects:
                 yield await self._ainvoke()
             yield await run_async(self._invoke)
 
-        def _aforward(self) -> Effect[Out]:
+        def _async_effect(self) -> Effect[Out]:
             return Effects.Async(self._ainvoke)
 
-        def _iter_forward(self) -> Effect[Out]:
+        def _iter_effect(self) -> Effect[Out]:
             return Effects.Iterator(self._iter)
 
-        def _aiter_forward(self) -> Effect[Out]:
+        def _async_iter_effect(self) -> Effect[Out]:
             return Effects.AsyncIterator(self._aiter)
 
     @final
@@ -238,7 +238,7 @@ class Effects:
             self._effects = effects
             self._max_workers = max_workers
 
-        def invoke(self) -> Out:
+        def invoke(self) -> dict[str, Any]:
             with get_executor(max_workers=self._max_workers) as executor:
                 futures = [
                     executor.submit(effect.invoke)
@@ -249,7 +249,7 @@ class Effects:
                     for k, future in tzip(self._effects, futures)
                 }
 
-        async def ainvoke(self) -> Out:
+        async def ainvoke(self) -> dict[str, Any]:
             generators = await asyncio.gather(*(
                 effect.ainvoke
                 for effect in self._effects.values()
@@ -259,11 +259,40 @@ class Effects:
                 for k, v in zip(self._effects, generators)
             }
 
-        def iter(self) -> Iterator[Out]:
-            pass
+        def iter(self) -> Iterator[dict[str, Any]]:
+            current = {}
 
-        async def aiter(self) -> AsyncIterator[Out]:
-            pass
+            def iterate_effect(name: str, effect: Effect[Any]) -> Iterator[dict[str, Any]]:
+                for chunk in effect.iter():
+                    current[name] = chunk
+                    yield current
+
+            with get_executor(max_workers=self._max_workers) as executor:
+                futures = [
+                    executor.submit(
+                        tpartial(iterate_effect, name, effect)
+                    )
+                    for name, effect in self._effects.items()
+                ]
+                for future in futures:
+                    yield from future.result()
+
+        async def aiter(self) -> AsyncIterator[dict[str, Any]]:
+            current = {}
+
+            async def aiterate_effect(name: str, effect: Effect[Any]) -> AsyncIterator[dict[str, Any]]:
+                async for chunk in effect.aiter(): # type: ignore
+                    current[name] = chunk
+                    yield current
+
+            generators = await asyncio.gather(*(
+                aiterate_effect(name, effect)
+                for name, effect in self._effects.items()
+            ))
+
+            for generator in generators:
+                async for output in generator:
+                    yield output
 
     @final
     class Map(Generic[Other, Out], Effect[Out]):
