4de68b34ea9c80081ca9fdea5d4b0a88495f9863
bengabay <ben@netop.cloud>
2024-07-02 03:45:06 +0300

    using index keyword extractor as default

--- modstack/modstack/query/retrievers/keyword_table.py
+++ modstack/modstack/query/retrievers/keyword_table.py
@@ -3,26 +3,29 @@ from typing import Optional
 
 from modstack.artifacts import Artifact
 from modstack.core import Module, ModuleLike, SerializableModule, coerce_to_module
-from modstack.query.common import simple_keyword_extractor
-from modstack.query.structs import KeywordTable
+from modstack.query.indices import KeywordTableIndex
 from modstack.typing import Effect, Effects
 
 class KeywordTableRetriever(SerializableModule[Artifact, list[Artifact]]):
     _keyword_extractor: Module[Artifact, set[str]]
 
     @property
-    def index(self) -> KeywordTable:
+    def index(self) -> KeywordTableIndex:
         return self._index
 
     def __init__(
         self,
-        index: KeywordTable,
+        index: KeywordTableIndex,
         keyword_extractor: Optional[ModuleLike[Artifact, set[str]]] = None,
         **kwargs
     ):
         super().__init__(**kwargs)
         self._index = index
-        self._keyword_extractor = coerce_to_module(keyword_extractor) if keyword_extractor else simple_keyword_extractor
+        self._keyword_extractor = (
+            coerce_to_module(keyword_extractor)
+            if keyword_extractor
+            else self.index.keyword_extractor
+        )
 
     def forward(self, artifact: Artifact, **kwargs) -> Effect[list[Artifact]]:
         return Effects.From(
