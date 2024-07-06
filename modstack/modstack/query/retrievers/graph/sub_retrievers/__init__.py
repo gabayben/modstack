4e67cde2719242c2f448ea02c73f552c27d974b4
bengabay <ben@netop.cloud>
2024-07-06 07:30:22 +0300

    added graph sub-retrievers

--- modstack/modstack/query/retrievers/graph/sub_retrievers/__init__.py
+++ modstack/modstack/query/retrievers/graph/sub_retrievers/__init__.py
@@ -1,1 +1,6 @@
 from .base import GraphSubRetriever
+from .custom import CustomGraphRetriever
+from .cypher_template import CypherTemplateRetriever
+from .llm_synonym import LLMSynonymGraphRetriever
+from .text_to_cypher import TextToCypherRetriever
+from .vector import VectorGraphRetriever
---
+++ modstack/modstack/query/retrievers/graph/sub_retrievers/custom.py
@@ -0,0 +1,9 @@
+from modstack.artifacts import Artifact
+from modstack.query.retrievers import GraphIndexQuery, GraphSubRetriever
+
+class CustomGraphRetriever(GraphSubRetriever):
+    def _retrieve(self, query: GraphIndexQuery, **kwargs) -> list[Artifact]:
+        pass
+
+    async def _aretrieve(self, query: GraphIndexQuery, **kwargs) -> list[Artifact]:
+        pass
